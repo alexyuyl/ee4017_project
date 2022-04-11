3f8865d1de449acd98afe16e7440a42111d80591
Alex Yu <alexyuyl21@gmail.com>
2022-04-12 00:12:53 +0800

    relocate @app methods under class User

--- class_user.py
+++ class_user.py
@@ -39,95 +39,87 @@ class User:
                     add += float(tx["amount"])
         self.myWallet.amount = 50 + add - minus
 
+    @app.get('/mine')
+    def mine(self):
+        # mine reward
+        self.blockchain.current_transactions.append(
+            {'sender': 'reward',
+             'recipient': self.myWallet.identity,
+             'amount': '1'}
+        )
+        self.myWallet.amount += 1
+        # add new block to the chain
+        block = self.blockchain.new_block()
 
-@app.get('/mine')
-def mine(self):
-    # mine reward
-    self.blockchain.current_transactions.append(
-        {'sender': 'reward',
-         'recipient': self.myWallet.identity,
-         'amount': '1'}
-    )
-    self.myWallet.amount += 1
-    # add new block to the chain
-    block = self.blockchain.new_block()
-
-    new_block = {
-        'message': 'New Block Mined',
-        'index': block['index'],
-        'transaction': block['transaction'],
-        'hash': block['hash'],
-        'prev_hash': block['prev_hash'],
-    }
-    self.update_balance()
-    return jsonable_encoder(new_block)
-
-
-@app.post('/new_transaction')
-def new_transaction(self, sender: str, recipient: str, amount: str):
-    # create a new transaction
-    tmp_amount = self.myWallet.tmp_amount
-    if tmp_amount <= float(amount) + 0.5:
-        response = {'message': 'Wallet Balance is not enough for transaction'}
-        return jsonable_encoder(response)
-    else:
-        self.myWallet.tmp_amount = tmp_amount - float(amount)
-    index = self.blockchain.new_transaction(sender, recipient, amount)
-    response = {'message': f'Transaction will be added to Block {index}'}
-    return jsonable_encoder(response)
-
-
-@app.get('/full_chain')
-def full_chain(self):
-    response = {
-        'chain': self.blockchain.chain,
-        'length': len(self.blockchain.chain),
-    }
-    return jsonable_encoder(response)
-
-
-@app.get('/get_pub_key')
-def get_pub_key(self):
-    return jsonable_encoder({'public_key': self.myWallet.identity})
-
-
-@app.get('/get_balance')
-def get_balance(self):
-    return jsonable_encoder({'Balance': "{:.2f}".format(self.myWallet.amount)})
-
-
-@app.get('/get_node')
-def get_node(self):
-    response = {
-        'total_nodes': list(self.blockchain.nodes),
-    }
-    return jsonable_encoder(response)
-
-
-@app.post('/register')
-def register(self, node: str):
-    self.blockchain.register_node(node)
-
-    response = {
-        'message': 'New nodes have been added',
-        'total_nodes': list(self.blockchain.nodes),
-    }
-    return jsonable_encoder(response)
-
-
-@app.get('/consensus')
-def consensus(self):
-    replaced = self.blockchain.consensus()
-
-    if replaced:
-        response = {
-            'message': 'Our chain was replaced',
-            'new_chain': self.blockchain.chain
+        new_block = {
+            'message': 'New Block Mined',
+            'index': block['index'],
+            'transaction': block['transaction'],
+            'hash': block['hash'],
+            'prev_hash': block['prev_hash'],
         }
         self.update_balance()
-    else:
+        return jsonable_encoder(new_block)
+
+    @app.post('/new_transaction')
+    def new_transaction(self, sender: str, recipient: str, amount: str):
+        # create a new transaction
+        tmp_amount = self.myWallet.tmp_amount
+        if tmp_amount <= float(amount) + 0.5:
+            response = {'message': 'Wallet Balance is not enough for transaction'}
+            return jsonable_encoder(response)
+        else:
+            self.myWallet.tmp_amount = tmp_amount - float(amount)
+        index = self.blockchain.new_transaction(sender, recipient, amount)
+        response = {'message': f'Transaction will be added to Block {index}'}
+        return jsonable_encoder(response)
+
+    @app.get('/full_chain')
+    def full_chain(self):
         response = {
-            'message': 'Our chain is authoritative',
-            'new_chain': self.blockchain.chain
+            'chain': self.blockchain.chain,
+            'length': len(self.blockchain.chain),
         }
-    return jsonable_encoder(response)
+        return jsonable_encoder(response)
+
+    @app.get('/get_pub_key')
+    def get_pub_key(self):
+        return jsonable_encoder({'public_key': self.myWallet.identity})
+
+    @app.get('/get_balance')
+    def get_balance(self):
+        return jsonable_encoder({'Balance': "{:.2f}".format(self.myWallet.amount)})
+
+    @app.get('/get_node')
+    def get_node(self):
+        response = {
+            'total_nodes': list(self.blockchain.nodes),
+        }
+        return jsonable_encoder(response)
+
+    @app.post('/register')
+    def register(self, node: str):
+        self.blockchain.register_node(node)
+
+        response = {
+            'message': 'New nodes have been added',
+            'total_nodes': list(self.blockchain.nodes),
+        }
+        return jsonable_encoder(response)
+
+    @app.get('/consensus')
+    def consensus(self):
+        replaced = self.blockchain.consensus()
+
+        if replaced:
+            response = {
+                'message': 'Our chain was replaced',
+                'new_chain': self.blockchain.chain
+            }
+            self.update_balance()
+        else:
+            response = {
+                'message': 'Our chain is authoritative',
+                'new_chain': self.blockchain.chain
+            }
+        return jsonable_encoder(response)
